a08adeb2983f38e4a0742292d2aaae3174c4ef61
Henry Doan <henrydoan96@gmail.com>
2020-06-25 21:19:46 -0600

    menu provider; ;

--- client/src/providers/DinerProvider.js
+++ client/src/providers/DinerProvider.js
@@ -47,7 +47,11 @@ class DinerProvider extends Component {
   }
 
   deleteDiner = (id) => {
-    
+    axios.delete(`/api/diners/${id}`)
+      .then( res => {
+        const { diners } = this.state
+        this.setState({ diners: diners.filter( d => d.id !== id )})
+      })
   }
 
   render() {
@@ -56,6 +60,7 @@ class DinerProvider extends Component {
         ...this.state,
         addDiner: this.addDiner,
         updateDiner: this.updateDiner,
+        deleteDiner: this.deleteDiner,
       }}>
         { this.props.children }
       </DinerContext.Provider>
