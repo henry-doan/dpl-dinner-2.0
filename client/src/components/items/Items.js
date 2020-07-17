88e5995adb83aed931b18e00e26a52317398da0d
Henry Doan <henrydoan96@gmail.com>
2020-07-17 13:08:30 -0600

    index show delete items

--- client/src/components/items/Items.js
+++ client/src/components/items/Items.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
 import { ItemConsumer } from '../../providers/ItemProvider';
 import ItemForm from './ItemForm';
+import { Card, Grid, Button } from 'semantic-ui-react';
 
 class Items extends Component {
+  componentDidMount() {
+    const { getAllItems, menuId } = this.props
+    getAllItems(menuId)
+  }
+
+  renderItems = () => {
+    const { updateItem, deleteItem, menuId } = this.props
+    return(
+      this.props.items.map( i => {
+        return(
+          <Grid.Column>
+            <Card>
+              <Card.Header>{i.name}</Card.Header>
+              <Card.Content>
+                {i.price}
+              </Card.Content>
+              <Card.Content extra>
+                <div className='ui two buttons'>
+                  <Button basic color='yellow'>
+                    Edit
+                  </Button>
+                  <Button basic color='red' onClick={() => deleteItem(menuId, i.id)}>
+                    Delete
+                  </Button>
+                </div>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+        )
+      })
+    )
+  }
 
   render() {
     const { menuId, addItem } = this.props
     return(
       <>
         <ItemForm menuId={menuId} addItem={addItem} />
+        <br />
+        <br />
+        <br />
+        <Grid columns={3} stackable>
+          { this.renderItems() }
+        </Grid>
       </>
     )
   }
